5788858000c41899e8808586dbadab776b61710c
Alexander Fields <Roku674@gmail.com>
2025-09-01 19:54:44 -0400

    Develop (#47)
    * Update PaginatedResponse.cs
    
    * 15
    
    * fix mongohelper

--- MC.Objects/API/PaginatedResponse.cs
+++ MC.Objects/API/PaginatedResponse.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Collections.Generic;
 
-namespace MandalaConsulting.Objects.API
+namespace MandalaConsulting.Objects
 {
     /// <summary>
     /// Represents a paginated response from an API
--- MC.Repository.Mongo/MongoHelper.cs
+++ MC.Repository.Mongo/MongoHelper.cs
@@ -234,41 +234,6 @@ namespace MandalaConsulting.Repository.Mongo
         }
 
         /// <summary>
-        /// Gets a paginated list of documents from the specified collection.
-        /// </summary>
-        /// <typeparam name="T">The type of documents to retrieve.</typeparam>
-        /// <param name="collectionName">The name of the collection.</param>
-        /// <param name="page">The page number (1-based).</param>
-        /// <param name="pageSize">The number of items per page.</param>
-        /// <param name="filter">Optional filter to match documents.</param>
-        /// <returns>A paginated response containing the documents and metadata.</returns>
-        public async Task<MandalaConsulting.Objects.API.PaginatedResponse<T>> GetPaginatedDocumentsAsync<T>(
-            string collectionName,
-            int page = 1,
-            int pageSize = 100,
-            FilterDefinition<T> filter = null)
-        {
-            IMongoCollection<T> collection = GetCollection<T>(collectionName);
-            
-            // Use empty filter if none provided
-            filter = filter ?? Builders<T>.Filter.Empty;
-            
-            // Get total count
-            long totalItems = await collection.CountDocumentsAsync(filter);
-            
-            // Calculate skip amount
-            int skip = (page - 1) * pageSize;
-            
-            // Get paginated data
-            List<T> data = await collection.Find(filter)
-                .Skip(skip)
-                .Limit(pageSize)
-                .ToListAsync();
-            
-            return new MandalaConsulting.Objects.API.PaginatedResponse<T>(data, page, pageSize, totalItems);
-        }
-
-        /// <summary>
         /// Gets documents from the specified collection that match the filter.
         /// </summary>
         /// <typeparam name="T">The type of documents to retrieve.</typeparam>
